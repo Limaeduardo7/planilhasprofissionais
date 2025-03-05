57133f2d75a77a353fcde93b05edeb4e46b84bfc
Seu Nome <seu@email.com>
2025-03-05 15:31:25 -0300

    Configurar GitHub Actions para deploy

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   images: {
     domains: ['i.imgur.com'],
     unoptimized: true,
-  }
+  },
+  // Adicionar um prefixo de ativos para o GitHub Pages, comentado inicialmente
+  // assetPrefix: '/planilhasprofissionais/',
+  // basePath: '/planilhasprofissionais',
 }
 
 module.exports = nextConfig 
